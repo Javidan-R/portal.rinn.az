abb0ecb0275b36c60b1e5761cdcdda74dd208aff
Javidan -R <rajablijavidan@gmail.com>
2024-02-07 02:33:26 +0400

    Update tsconfig

--- src/components/CategoriesCard/index.tsx
+++ src/components/CategoriesCard/index.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
 interface CategoriesCardProps {
-    image:string,
-  title:string,
-  desc:string,
-  categoriesId:number,
+  image: string;
+  title: string;
+  desc: string;
+  categoriesId: number;
 }
 const CategoriesCard: FC<CategoriesCardProps> = ({
   image,
@@ -12,24 +12,29 @@ const CategoriesCard: FC<CategoriesCardProps> = ({
   desc,
   categoriesId,
 }) => {
-
   return (
     <Link to={`/categories/${categoriesId}`}>
-
-  
-        <div className={`p-5 bg-white border border-solid border-gray-200  cursor-pointer shadow-md    w-full flex items-center w-full flex items-center rounded-lg  h-[104px] lg:h-[120px]`}>
-          <div className="cursor-pointer text-gray-900  flex items-center ">
-            <div className="flex-none">
-              <img src={image} alt="icon" className="h-12 w-12 object-contain" />
-            </div>
-             <article className="ml-4  w-60 text-wrap">
-          <div className="font-bold overflow-hidden truncate text-base mb-1">{title}</div>
-          <div className="w-72 line-clamp-2 max-w-full " style={{fontSize: '.75rem'}}>{desc}</div>
-        </article>
+      <div
+        className={`p-5 bg-white border border-solid border-gray-200  cursor-pointer shadow-md    w-full flex items-center w-full flex items-center rounded-lg  h-[104px] lg:h-[120px]`}
+      >
+        <div className="cursor-pointer text-gray-900  flex items-center ">
+          <div className="flex-none">
+            <img src={image} alt="icon" className="h-12 w-12 object-contain" />
           </div>
+          <article className="ml-4  w-60 text-wrap">
+            <div className="font-bold overflow-hidden truncate text-base mb-1">
+              {title}
+            </div>
+            <div
+              className="break-words max-w-full "
+              style={{ fontSize: ".75rem" }}
+            >
+              {desc}
+            </div>
+          </article>
         </div>
-      </Link>
-
+      </div>
+    </Link>
   );
 };
 
