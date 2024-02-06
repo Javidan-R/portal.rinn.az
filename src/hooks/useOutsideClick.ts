6bc410cd64c743529e130c75509ced5abb960c9e
Javidan -R <rajablijavidan@gmail.com>
2024-02-07 00:51:31 +0400

    http update and hooks

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -1,25 +1,25 @@
-import { useEffect, useRef, RefObject } from "react";
+// import { useEffect, useRef, RefObject } from "react";
 
-type OutsideClickHandler = () => void;
+// type OutsideClickHandler = () => void;
 
-export function useOutsideClick(
-  handler: OutsideClickHandler,
-  listenCapturing = true
-): RefObject<HTMLDivElement> {
-  const ref = useRef<HTMLDivElement>(null);
+// export function useOutsideClick(
+//   handler: OutsideClickHandler,
+//   listenCapturing = true
+// ): RefObject<HTMLDivElement> {
+//   const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    function handleClick(e: MouseEvent) {
-      if (ref.current && !ref.current.contains(e.target as Node)) {
-        handler();
-      }
-    }
+//   useEffect(() => {
+//     function handleClick(e: MouseEvent) {
+//       if (ref.current && !ref.current.contains(e.target as Node)) {
+//         handler();
+//       }
+//     }
 
-    document.addEventListener("click", handleClick, listenCapturing);
+//     document.addEventListener("click", handleClick, listenCapturing);
 
-    return () =>
-      document.removeEventListener("click", handleClick, listenCapturing);
-  }, [handler, listenCapturing]);
+//     return () =>
+//       document.removeEventListener("click", handleClick, listenCapturing);
+//   }, [handler, listenCapturing]);
 
-  return ref;
-}
+//   return ref;
+// }
